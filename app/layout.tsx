60ac50ecb305d8496ad6b0b65d7dd9f9b69d0f96
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-08-13 13:58:00 +0200

    Sync all

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// Chapter 2 CSS Styling -- Global styles, add by importing global.css into layout file
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
